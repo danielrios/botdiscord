f76cb735bb20debc4b9760a22ab7613e6f8e63ff
Daniel Rios <49802471+Dederios@users.noreply.github.com>
2020-04-30 13:36:37 -0300

    Create Fist Command

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 bot.once('ready', () => {
     console.log('Pronto!');
 })
 
-bot.login(config.token);
+bot.login(token);
+
+bot.on('message', msg => {
+    if (!msg.content.startsWith(prefix) || message.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const comando = args.shift().toLowerCase();
+
+    if (comando === "oi") {
+        msg.channel.send(`Olá ${msg.author.username}!`);
+    }
+});
